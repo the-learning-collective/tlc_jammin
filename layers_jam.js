c039deae9696b8d7fd340105bb78d14fb49300e5
Austin Guest <guest.austin@gmail.com>
2014-09-19 01:56:53 -0400

    let the jamming begin

---
+++ layers_jam.js
@@ -0,0 +1,14 @@
+function Layers(start, end){
+  this.layers = initLayers(start, end)
+
+  function initLayers(start, end){
+    var arr = []
+    years = _.range(start, end, 1)
+    _.each(years, function(year){
+      layer = L.
+        // whole bunch
+      arr.push(layer)
+    })
+    return arr
+  }
+}
